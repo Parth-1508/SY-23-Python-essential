19e445dff9456b3bd3773e9320a2305758178b0a
Parth Prashant Pate <parth.p.pate07@gmail.com>
2026-07-17 12:12:01 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,23 @@
+n1=int(input("Enter a number:"))
+n2=int(input("Enter another number:"))
+
+n3=n1+n2
+print("\nSimple calculations:\nThe sum of ",n1,"and",n2,"is:",n3)#sum
+
+n3=n1-n2
+print("The difference of ",n1,"and",n2,"is:",n3)#sub
+
+n3=n1*n2
+print("The product of ",n1,"and",n2,"is:",n3)#mult
+
+n3=n1/n2
+print("The quotient of ",n1,"and",n2,"is:",n3)#div
+
+n3=n1**n2
+print("\nComplex calculations:\nThe exponential value of ",n1,"and",n2,"is:",n3)#exponent
+
+n3=n1%n2
+print("The modulus of ",n1,"and",n2,"is:",n3)#mod
+
+n3=n1//n2
+print("The floor division of ",n1,"and",n2,"is:",n3)#floor div
