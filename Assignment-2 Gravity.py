0b8b87e4ee94fe784dcc1ef83210f821ae947b1b
Parth Prashant Pate <parth.p.pate07@gmail.com>
2026-07-24 11:06:52 +0530

    Add files via upload

---
+++ Assignment-2 Gravity.py
@@ -0,0 +1,16 @@
+# Constants Identifiers
+Earth_Gravity = 9.8
+Moon_Gravity = (1/6) * 9.8
+
+# User defined function
+def calculate_weight(mass, gravity):
+    return mass * gravity
+
+# Main execution
+mass = float(input("Enter the mass of the object in kg: "))
+
+earth_weight = calculate_weight(mass, Earth_Gravity)
+moon_weight = calculate_weight(mass, Moon_Gravity)
+
+print("Weight on Earth: ", earth_weight, "N")
+print("Weight on Moon: ", moon_weight, "N")
